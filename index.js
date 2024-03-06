582014f3d876119f6475802bc60468795172fe3c
ghost237-sys <mwasczar03@gmail.com>
2024-03-07 01:59:11 +0300

    finished up the sidebar

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-function myFunction(){
-    var x = document.getElementById("sidebar");
-    if(x.style.display === "block"){
-        x.style.display = "none";
-    }else{
-        x.style.display = "block";
-    }
-}
+const body = document.querySelector("body"),
+    sidebar = body.querySelector(".sidebar"),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modewSwitch = body.querySelector(".mode"),
+    modeText = body.querySelector(".mode-text");
+
+    toggle.addEventListener("click",() =>{
+        sidebar.classList.toggle("close");
+
+    });
+
+    searchBtn.addEventListener("click",() =>{
+        sidebar.classList.remove("close");
+
+    });
+
+
+
+    modewSwitch.addEventListener("click",() =>{
+        body.classList.toggle("dark");
+
+        if(body.classList.contains("dark")){
+            modeText.innerText = "Light Mode"
+        }else{
+            modeText.innerText = "Dark Mode"
+        }
+
+        
+    });
